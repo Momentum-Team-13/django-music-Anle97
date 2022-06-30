103334a564129fde8cee1ba017036ae8165b24e6
An <an.tle.914@gmail.com>
2022-06-30 15:53:01 -0400

    Working file with no CSS. Need to add delete function

--- django_music/urls.py
+++ django_music/urls.py
@@ -7,12 +7,18 @@ from django_music import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('__debug__/', include ('debug_toolbar.urls')),
+
     path('', albums_views.list_albums, name='list_albums'),
-    path('/albums/new', albums_views.add_album, name='add_album'),
-    path('albums/<int:pk>', albums_views.album_detail, name='album_detail'),
-    path('albums/<int:pk>/edit', albums_views.edit_album, name='edit_album'),
-    path('albums/<int:pk>/delete', albums_views.delete_album, name='delete_album'),
+
+    path('albums/new', albums_views.add_album, name='add_album'),
+
+    path('albums/<int:pk>/', albums_views.album_details, name='album_details'),
+
+    path('albums/<int:pk>/edit/', albums_views.edit_album, name='edit_album'),
+
+    # path('albums/<int:pk>/delete/', albums_views.delete_album, name='delete_album'),
 
 ]
 
